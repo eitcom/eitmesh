5f2e2c59400ed3c5c927017d177d8e9870a3a02a
byliu <liubenyuan@gmail.com>
2022-02-17 13:09:02 +0800

    reorganize folder structure

---
+++ eitmesh/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Benyuan Liu"
+__author_email__ = "liubenyuan@gmail.com"
+__license__ = "BSD"
+APP_NAME = "eitmesh"
+APP_VERSION = "0.0.1"
+APP_DESCRIPTION = "A Mesh Package for pyEIT"
---
+++ setup.py
@@ -0,0 +1,26 @@
+# copied from https://github.com/adrienverge/yamllint/
+import pathlib
+from setuptools import setup
+
+from eitmesh import (
+    __author__,
+    __author_email__,
+    __license__,
+    APP_NAME,
+    APP_VERSION,
+    APP_DESCRIPTION,
+)
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+    name=APP_NAME,
+    version=APP_VERSION,
+    author=__author__,
+    author_email=__author_email__,
+    description=APP_DESCRIPTION.split("\n")[0],
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license=__license__,
+)
